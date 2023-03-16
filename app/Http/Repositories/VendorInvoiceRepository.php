055490013fa6c686fd58d1b49cedaca02af11c7f
bagusuntoro <untorobagus65@gmail.com>
2023-03-16 10:27:14 +0700

    prepare repositories

---
+++ app/Http/Repositories/VendorInvoiceRepository.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Http\Repositories;
+
+use App\Helpers\MongoModel;
+
+class HistoryRepository
+{
+    private MongoModel $historyModel;
+    public function __construct()
+    {
+        $this->historyModel = new MongoModel('history');
+    }
+
+    public function getId(string $id)
+    {
+        $result = $this->historyModel->get(['instruction_id' => $id]);
+    }
+
+    public function create(array $data)
+    {
+        $history[] = [
+            '_id' => (string) new \MongoDB\BSON\ObjectId(),
+            'activity' => $data['activity'],
+      'by_user' => $data['user'],
+      'timestamp' => $data['timestamp'],
+        ];
+
+        $insertData = [
+            'instruction_id' => $data['instruction_id'],
+            'history_data' => $history
+          ];
+      
+          $this->historyModel->save($insertData);   
+    }
+
+    public function save(array $data)
+  {
+    $this->historyModel->save($data);
+  }
+}
+?>
